f1e51e1337169a3683f3f5f2b989e3d5a9160426
kevinfiol <me@kevinfiol.com>
2024-11-21 23:56:43 -0500

    this function can live outside of the component

--- src/client/csv-export.tsx
+++ src/client/csv-export.tsx
@@ -9,36 +9,34 @@ function triggerCSVDownload(csv = '') {
   URL.revokeObjectURL(link.href);
 }
 
-export const CSVExport = () => {
-  const submitForm = async (formEl: HTMLFormElement) => {
-    const formData = new FormData(formEl);
-    const begin = formData.get('begin') as string;
-    const end = formData.get('end') as string;
-    const params = new URLSearchParams({ begin, end });
+const submitForm = async (formEl: HTMLFormElement) => {
+  const formData = new FormData(formEl);
+  const begin = formData.get('begin') as string;
+  const end = formData.get('end') as string;
+  const params = new URLSearchParams({ begin, end });
 
-    try {
-      const res = await fetch(`${CSV_EXPORT_ENDPOINT}?${params.toString()}`);
-      const text = await res.text();
-      triggerCSVDownload(text);
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
-  return (
-    <form
-      onSubmit={(ev) => {
-        ev.preventDefault();
-        submitForm(ev.target as HTMLFormElement);
-      }}
-    >
-      <h2>CSV Export</h2>
-      <p>(Optional) Enter a date range to filter constituent signup time</p>
-      <fieldset role="group">
-        <input type="date" name="begin" />
-        <input type="date" name="end" />
-        <button type="submit">Export</button>
-      </fieldset>
-    </form>
-  );
+  try {
+    const res = await fetch(`${CSV_EXPORT_ENDPOINT}?${params.toString()}`);
+    const text = await res.text();
+    triggerCSVDownload(text);
+  } catch (e) {
+    console.error(e);
+  }
 };
+
+export const CSVExport = () => (
+  <form
+    onSubmit={(ev) => {
+      ev.preventDefault();
+      submitForm(ev.target as HTMLFormElement);
+    }}
+  >
+    <h2>CSV Export</h2>
+    <p>(Optional) Enter a date range to filter constituent signup time</p>
+    <fieldset role="group">
+      <input type="date" name="begin" />
+      <input type="date" name="end" />
+      <button type="submit">Export</button>
+    </fieldset>
+  </form>
+);
